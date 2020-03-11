f45594ad312ec4bf14bb78c47fbef5eaa973cdf5
Nuno <nfnvsc@hotmail.com>
2020-03-11 16:18:53 +0000

    Finished processInput

--- main.cpp
+++ main.cpp
@@ -1,19 +1,41 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+
 using namespace std;
 
 typedef struct vertice{
     int val;
+    int num_connected;
     vertice** connected;
 } vertice;
 
 vertice** vertice_array;
 
+vertice* get_vertice(int index){
+    return vertice_array[index-1];
+}
+
+void new_vertice(int value, int size, int index){
+    vertice* new_vert;
+
+    new_vert = (vertice*)malloc(sizeof(vertice));
+    new_vert->val = value;
+    new_vert->num_connected = 0;
+    new_vert->connected = (vertice**)malloc(sizeof(vertice*) * size);
+
+    vertice_array[index] = new_vert;
+
+}
+
+void add_connection(int index, vertice* new_connection){
+    vertice* aux_vert = get_vertice(index);
+    aux_vert->connected[aux_vert->num_connected++] = new_connection;
+}
+
 void processInput(string file_name){
     string line;
     ifstream myfile(file_name);
-    vertice new_vert;
 
     int first, second;
     int i, aux1, aux2;
@@ -22,21 +44,21 @@ void processInput(string file_name){
         getline(myfile,line);
         sscanf(line.c_str(), "%d,%d", &first, &second);
         
-        vertice_array = (vertice **)malloc(sizeof(vertice) * stoi(line));
+        vertice_array = (vertice **)malloc(sizeof(vertice*) * first);
 
         for(i = 0; i < first; i++){
             getline(myfile, line);
             sscanf(line.c_str(), "%d", &aux1);
 
-            new_vert = malloc(sizeof(vertice));
+            new_vertice(aux1, first, i);
         }
 
         for(i = 0; i < second; i++){
             getline(myfile, line);
             sscanf(line.c_str(), "%d %d", &aux1, &aux2);
-        }
 
-        printf("%d, %d", first, second);
+            add_connection(aux1, get_vertice(aux2));
+        }
 
         myfile.close();
     }
