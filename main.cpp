de6b1a565be7db6580132e7b2034b19dbf379980
Miguel Silva <miguel04silva18@gmail.com>
2020-03-13 15:31:43 +0000

    Algorithm 70%

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "tarjan.h"
-
+#include <iostream>
+#include <bits/stdc++.h>
+#define NIL -1
 using namespace std;
 
-typedef struct vertice{
+typedef struct vertice {
     int val;
     int num_connected;
-    int* connected;
+    int *connected;
 } vertice;
 
 vertice** vertice_array;
@@ -16,6 +17,10 @@ int graphSize;
 
 vertice* get_vertice(int index){
     return vertice_array[index-1];
+}
+
+int min(int a, int b) {
+    return a < b ? a : b;
 }
 
 void new_vertice(int value, int size, int index){
@@ -69,8 +74,82 @@ void processInput(string file_name){
     else cout << "Unable to open file"; 
 }
 
+void printGraph(vertice **graph, int graphSize) {
+    for (int i = 0; i < graphSize; i++)
+    {
+        cout << graph[i]->val << "\n";
+    }
+}
+
+void giveValues(vertice **scc, int num_nodes)
+{
+    int maxValue = 0;
+    for (int i = 0; i < num_nodes; i++) {
+        if (scc[i]->val > maxValue)
+        {
+            maxValue = scc[i]->val;
+        }
+    }
+    for (int i = 0; i < num_nodes; i++) {
+        scc[i]->val = maxValue;
+    }
+}
+
+void tarjanSCC(int vertex, int disc[], int low[], stack<int> &stack, bool inStack[], vertice **graph) {
+    static int dtime = 0;
+
+    disc[vertex] = low[vertex] = ++dtime;
+    stack.push(vertex);
+    inStack[vertex] = true;
+
+    for (int i = 0; i < graph[vertex]->num_connected; i++) {
+        int ver = graph[vertex]->connected[i];
+        if (disc[ver] == NIL) {
+            tarjanSCC(ver, disc, low, stack, inStack, graph);
+            low[vertex] = min(low[vertex], low[ver]);
+        }
+        else if (inStack[ver] == true)
+            low[vertex] = min(low[vertex], disc[ver]);
+    }
+
+    //Pop the found SCC and give highest value to the components
+    int w = 0;
+    int index = 0;
+    vertice **scc;
+    if (low[vertex] == disc[vertex]) {
+        while (stack.top() != vertex) {
+            w = (int)stack.top();
+            scc[index++] = get_vertice(w);
+            inStack[w] = false;
+            stack.pop();
+        }
+        w = (int)stack.top();
+        scc[index++] = get_vertice(w);
+        inStack[w] = false;
+        stack.pop();
+    }
+    giveValues(scc, index);
+}
+
+void tarjan(vertice **graph, int u) { // u = number of nodes
+    int *d = new int[u];
+    int *low = new int[u];
+    bool *inStack = new bool[u];
+    stack<int> s;
+
+    for (int i = 0; i < u; i++) {
+        d[i] = low[i] = NIL;
+        inStack[i] = false;
+    }
+    for (int i = 0; i < u; i++) {
+        if (d[i] == NIL)
+            tarjanSCC(i, d, low, s, inStack, graph);
+    }
+}
+
 int main(){
-    processInput("teste.txt");
+    processInput("T01_clique.in");
     tarjan(vertice_array, graphSize);
+    cout << "algorithm completed" << "\n";
     return 0;
 }
