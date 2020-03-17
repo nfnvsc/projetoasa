f5c329c15c62d2cd0b730bcbd2dbf4cbc887cda1
Nuno <nfnvsc@hotmail.com>
2020-03-17 19:50:23 +0000

    Fixes

--- main.cpp
+++ main.cpp
@@ -16,11 +16,11 @@ int max_val = 0;
 class Graph{
     int v;
     list<int> *adjList; /*list of adjacent vertexs*/
-    int *vertList;
-    int *maxVisit;
+    int *vertList; /*list of vertex values*/
+    int *maxVisit; /*max number of time a vertex can be visited*/
 
     void dfs();
-    int aux(int vertex, int *visited);
+    int nextVisit(int vertex, int *visited);
     void dfsSearch(int vertex, int *visited);
 
 public:
@@ -45,8 +45,6 @@ void Graph::addVertex(int val, int i){
 
 void Graph::addConnection(int x, int y){
     adjList[x].push_back(y); 
-
-    //if (!adjList[y].empty())
     maxVisit[y] += 1;
 }
 
@@ -57,43 +55,37 @@ void Graph::printGraph(){
 }
 
 void Graph::dfs() {
-     //nao devia ser global
     int *visited = new int[v]; 
     for (int i = 0; i < v; i++){
         visited[i] = 0; 
         if(adjList[i].empty())
-            maxVisit[i] = 1;
-        //cout << "adList: " << *adjList[i].begin()<< "\n";
-        //cout << "Visit: " << maxVisit[i]<< "\n\n";
+            maxVisit[i] -= 1;
     } 
-    
-    //cout << "vert 4: " << maxVisit[3] << "\n";
 
     for (int i = 0; i < v; i++){
         max_val = 0;
-        if (visited[i] < maxVisit[i])
+        if ((visited[i] < maxVisit[i]) && (!adjList[i].empty()))
             dfsSearch(i, visited);
     }
-        
-        
-}
 
-int Graph::aux(int vertex, int *visited){
+}   
+
+int Graph::nextVisit(int vertex, int *visited){
     int vert = -1;
     list<int>::iterator i;
     
     for(i = adjList[vertex].begin(); i != adjList[vertex].end(); i++){
         if(visited[*i] < maxVisit[*i]){ //first avaliable vertex
-            vert = *i;
+            vert = *i; 
             break;
         }
     }
     
-    if(vert == -1) return -1;
-
     for(;i != adjList[vertex].end();i++)
         if((visited[*i] < visited[vert]) && (visited[*i] < maxVisit[*i])) //get the vertex with least visits
             vert = *i;
+
+    adjList[vertex].remove(vert); //no longer needs to be visited
 
     return vert;
 }
@@ -106,7 +98,7 @@ void Graph::dfsSearch(int vertex, int *visited) {
 
     int vert;
 
-    while((vert = aux(vertex, visited)) != -1){
+    while((vert = nextVisit(vertex, visited)) != -1){
 
         dfsSearch(vert, visited);
         //cout << vertex + 1 << " ";
@@ -119,81 +111,31 @@ void Graph::dfsSearch(int vertex, int *visited) {
     vertList[vertex] = max_val;
 }
 
-void processInputFile(string file_name){
-    string line;
-    ifstream myfile(file_name);
-    int first, second;
-    int aux1, aux2;
-
-    if (myfile.is_open()){
-        getline(myfile,line);
-        sscanf(line.c_str(), "%d,%d", &first, &second);
-
-        Graph graph(first);
-
-        for(int i = 0; i < first; i++){
-            getline(myfile, line);
-            sscanf(line.c_str(), "%d", &aux1);
-
-            graph.addVertex(aux1, i);
-        }
-
-        for(int i = 0; i < second; i++){
-            getline(myfile, line);
-            sscanf(line.c_str(), "%d %d", &aux1, &aux2);
-
-            graph.addConnection(aux1 - 1, aux2 - 1);
-        }
-        graph.printGraph();
-        myfile.close();
-    }
-
-    else cout << "Unable to open file"; 
-}
-
-
 void processInput(){
     int first, second;
     int aux1, aux2;
-    while(1){
 
-        if (scanf("%d,%d", &first, &second) == 0) return;
+    if (scanf("%d,%d", &first, &second) == 0) return;
 
-        Graph graph(first);
+    Graph graph(first);
 
-        for(int i = 0; i < first; i++){
+    for(int i = 0; i < first; i++){
 
-            if(scanf("%d", &aux1) == 0) return;
+        if(scanf("%d", &aux1) == 0) return;
 
-                graph.addVertex(aux1, i);
-        }
-
-        for(int i = 0; i < second; i++){
-            if(scanf("%d %d", &aux1, &aux2) == 0) return;
-
-            graph.addConnection(aux1 - 1, aux2 - 1);
-        }
-        graph.printGraph();
+            graph.addVertex(aux1, i);
     }
+
+    for(int i = 0; i < second; i++){
+        if(scanf("%d %d", &aux1, &aux2) == 0) return;
+
+        graph.addConnection(aux1 - 1, aux2 - 1);
+    }
+    graph.printGraph();
     
 }
 
-
 int main(){
-
     processInput();
-    cout << "XXXXXXXX\n";
-
-    processInputFile("T01_clique.in");
-    cout << "XXXXXXXX\n";
-    processInputFile("T02_tree_with_prop.in");
-    cout << "XXXXXXXX\n";
-    processInputFile("T03_tree_no_prop.in");
-    cout << "XXXXXXXX\n";
-    processInputFile("T04_small_no_prop.in");
-    cout << "XXXXXXXX\n";
-    processInputFile("T05_disconnected.in");
-
-
     return 0;
 }
