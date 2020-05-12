62553b57f5ff867b3303d4e1fbbb9bd9fbbfbc86
nfnvsc <nfnvsc@hotmail.com>
2020-05-12 17:52:54 +0100

    Fix

--- main2.cpp
+++ main2.cpp
@@ -38,8 +38,9 @@ public:
     int maximumFlow()
     { // Edmonds–Karp
 
-        int maximumFlow = 0;
-        vector<Edge *> parentEdges(numberOfVertices, nullptr);
+        int maximumFlow = 0;   
+
+        vector<Edge *> parentEdges(numberOfVertices, nullptr);        
         deque<int> bfsQueue;
 
         // Do a BFS to find every path from source to sink
@@ -49,15 +50,19 @@ public:
 
             int current = bfsQueue.front();
             bfsQueue.pop_front();
+            //parentEdges[current] = 0;
             for (auto &edge : adjacencyList[current])
             {
+                if (parentEdges[edge.v] != NULL) continue; //prevent it from going backwards
 
+                printf("Parent of %d is %d\n", edge.v, edge.u);
                 parentEdges[edge.v] = &edge;
                 bfsQueue.push_back(edge.v);
 
                 if (edge.v == sink)
-                {
-                    printf("encontrou saida\n");
+                {   
+                    
+                    printf("encontrou saida : %d\n", edge.v);
 
                     // A path was found from source to sink
                     // Find the maximum flow on this path
@@ -67,6 +72,7 @@ public:
                     {
                         maxFlow = min(maxFlow, parentEdge->cap - parentEdge->flux);
                         parentEdge = parentEdges[parentEdge->u];
+                        //printf("\n%d", parentEdge->v);
                     }
 
                     maximumFlow += maxFlow;
@@ -98,27 +104,33 @@ void processInput()
         return;
     if (scanf("%d %d", &S, &C) == 0)
         return;
-    Graph graph((M * N) +2);
+    Graph graph((M * N)*2 + 2);
+
     for (int i = 1; i < M * N + 1; i++)
     {
+        //graph.addEdge(i,i);
         //right
-        if (i % N != 0)
+        if (i % N != 3){
+            printf("%d\n", i);
             graph.addEdge(i, i + 1);
+            graph.addEdge(i + 1, i);
+        }
 
         //under
-        if (i < M * (N - 1))
+        if (i < M * (N - 1)){
             graph.addEdge(i, i + M);
+            graph.addEdge(i + M, i);
+        }
     }
     for (int i = 0; i < S; i++)
     {
-        if (scanf("%d %d", &x, &y) == 0)
-            return;
+        if (scanf("%d %d", &x, &y) == 0) return;
+       
         graph.addEdge(M * (y - 1) + x, M * N + 1); //supermercados apontam para o target
     }
     for (int i = 0; i < C; i++)
     {
-        if (scanf("%d %d", &x, &y) == 0)
-            return;
+        if (scanf("%d %d", &x, &y) == 0) return;
         graph.addEdge(0, M * (y - 1) + x); //source aponta para as casas 
     }
 
